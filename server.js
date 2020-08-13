d288ca7dc8bcbf4c6aa7f4a82bd4be47f9094e5d
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-08-13 11:42:08 -0700

    fixed dependency typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var logger = require('logger');
+var logger = require('morgan');
 
 var express = require('express');
 var app = express();
